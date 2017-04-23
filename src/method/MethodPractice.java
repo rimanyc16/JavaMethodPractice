06276b7b3924193eaee9adcf43f64370e0f04b32
Sharmin Akter Rima <rimanyc16@gmail.com>
2017-04-23 11:31:28 -0400

    add new class and main method

---
+++ src/method/MethodPractice.java
@@ -0,0 +1,18 @@
+package method;
+
+public class MethodPractice {
+
+	public static void main(String[] args) {
+
+	System.out.println("");	
+		
+	}
+	/**
+	 * Que 1:
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+}
